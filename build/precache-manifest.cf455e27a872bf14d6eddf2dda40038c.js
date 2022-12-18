ffc4164cfc95b63ff487a82f3b6b6055de71c7f2
Yuan <yuanji.yj@gmail.com>
2022-12-18 11:35:51 -0500

    initial setup

---
+++ build/precache-manifest.cf455e27a872bf14d6eddf2dda40038c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a932a1246af8960a57962df76d3be1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c595a6127a5204b20ca",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de6ea566eadd15775e89",
+    "url": "/static/js/2.adb64591.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.adb64591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8474d8f97bd84cfb6351",
+    "url": "/static/js/3.78319ed7.chunk.js"
+  },
+  {
+    "revision": "7c595a6127a5204b20ca",
+    "url": "/static/js/main.07a9e552.chunk.js"
+  },
+  {
+    "revision": "832acd10b51fef051f42",
+    "url": "/static/js/runtime-main.162e2586.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/static/media/svg-3.397c455a.svg"
+  }
+]);
