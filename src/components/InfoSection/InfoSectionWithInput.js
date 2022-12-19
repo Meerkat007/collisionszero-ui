ce1a83e85af93884ee2526d7be19b3465f0fc87f
Yuan <yuanji.yj@gmail.com>
2022-12-18 19:20:03 -0500

    text

--- src/components/InfoSection/InfoSectionWithInput.js
+++ src/components/InfoSection/InfoSectionWithInput.js
@@ -44,7 +44,7 @@ import {FeedbackView} from '..'
                         <FormInput
                             name='plateNumber' 
                             type='text' 
-                            placeholder='' 
+                            placeholder='Plate Num/Bumper ID' 
                             onChange={(event) => {
                                 setPlateNum(event.target.value)
                             }}
